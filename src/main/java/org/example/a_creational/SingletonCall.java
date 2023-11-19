1e41150c845729dc604a6e9e79148bc2c1e37863
Yungil Kim <wowwow0124@naver.com>
2023-11-19 14:28:52 +0900

    ex

--- src/main/java/org/example/a_creational/SingletonCall.java
+++ src/main/java/org/example/a_creational/SingletonCall.java
@@ -1,19 +1,35 @@
 package org.example.a_creational;
 
 import org.example.a_creational.singleton.SimpleSingleton;
+import org.example.a_creational.singleton.Singleton;
 
+/**
+ * 프로그래밍 기법 : Singleton
+ */
 public class SingletonCall {
     SimpleSingleton simpleSingleton = SimpleSingleton.getOb();
+    Singleton singleton = Singleton.getKey();
 
+    /**
+     * 1번 방식의 경우에는 클래스 로딩시 인스턴스를 생성한다. 빠르게 접근할 수 있으나,
+     * 클래스 호출 시 항상 인스턴스가 생성되므로 자원낭비가 발생할 수 있다.
+     *
+     * 그에 반하여 2번 방식은, 클래스 호출 시 항상 같은 인스턴스를 Return 하므로 성능적으로 이점이 있다.
+     *
+     * @see : https://readystory.tistory.com/116
+     */
     public static void main(String[] args) {
+        SingletonCall S = new SingletonCall();
 
-
+        S.simpleSingletonExe();     //1번 예시
+        S.singleton();              //2번 예시
 
 
     }
-    public void SimpleSingletonExe() {
-        simpleSingleton.getEmailId();
-        simpleSingleton.getPasswordSMTP();
-        simpleSingleton.getPort();
+    public String simpleSingletonExe() {
+        return simpleSingleton.getPort();
+    }
+    public String singleton() {
+        return singleton.getSecretKey();
     }
 }
--- src/main/java/org/example/a_creational/singleton/SimpleSingleton.java
+++ src/main/java/org/example/a_creational/singleton/SimpleSingleton.java
@@ -5,7 +5,7 @@ package org.example.a_creational.singleton;
  * private 생성자 만을 정의하여 외부 클래스로부터 인스턴스 생성을 차단하는 방식이다.
  *
  * --- 예시 상황을 만들었다. ---
- * SMTP 무료로 제공하는 서비스를 제공받았으며, 그 서비스를 이용하기 위한 소스를 싱글톤으로 작성해본다.
+ * SMTP 무료로 제공하는 서비스를 제공받았으며, 그 서비스를 이용하기 위한 포트를 싱글톤으로 작성해본다.
  */
 public class SimpleSingleton {
     private String port;
@@ -16,8 +16,6 @@ public class SimpleSingleton {
 
     private SimpleSingleton() {
         this.port = "1234"; //SMTP 포트
-        this.emailId = "what@Example.yikim.com";
-        this.passwordSMTP = "A1QrEgS3413WaE";
     }
 
     public static SimpleSingleton getOb() {
@@ -26,11 +24,5 @@ public class SimpleSingleton {
 
     public String getPort() {
         return this.port;
-    }
-    public String getEmailId() {
-        return this.emailId;
-    }
-    public String getPasswordSMTP() {
-        return this.passwordSMTP;
     }
 }
