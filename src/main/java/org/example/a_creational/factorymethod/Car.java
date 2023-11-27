436dad86b1fce5cdbe673f6b408f177931af2d7e
Yungil Kim <wowwow0124@naver.com>
2023-11-27 22:02:31 +0900

    Factory Method Interface

---
+++ src/main/java/org/example/a_creational/factorymethod/Car.java
@@ -0,0 +1,11 @@
+package org.example.a_creational.factorymethod;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Car {
+    Optional<List> AVANTE_CN7();
+    Optional<List> SONATA();
+    Optional<List> Grandeur();
+    Optional<List> Zenesis();
+}
