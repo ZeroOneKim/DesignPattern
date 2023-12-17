ff079e1296bfc913fd2914b0b4f05e8d7a0f956c
Yungil Kim <wowwow0124@naver.com>
2023-12-17 21:41:32 +0900

    user org

--- src/main/java/org/example/c_behavior/observer/User1.java
+++ src/main/java/org/example/c_behavior/observer/User1.java
@@ -3,6 +3,6 @@ package org.example.c_behavior.observer;
 public class User1 implements Says {
     @Override
     public void say(String sentence) {
-        System.out.println("요청 사항 : " + sentence);
+        System.out.println("User1 : " + sentence);
     }
 }
