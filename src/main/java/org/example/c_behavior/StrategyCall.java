3b48924afbf5ababc964d2db942ec9b1cdf694ca
Yungil Kim <wowwow0124@naver.com>
2023-12-26 12:12:17 +0900

    이론 학습

---
+++ src/main/java/org/example/c_behavior/StrategyCall.java
@@ -0,0 +1,16 @@
+package org.example.c_behavior;
+
+/**
+ * Strategy Pattern -> 알고리즘을 정의하고 각각을 캡슐화 하며, 이를 상호 교환가능하게 만드는 패턴이다.
+ * 구성요소
+ * - Context  : 인터페이스 제공
+ * - Strategy : 알고리즘을 나타내는 인터페이스 정의
+ *
+ * 유연하게 확장이 가능하며 코드 재사용성이 높아지지만, 클래스가 많아지면 복잡성이 증가 할 수 있음.
+ *
+ * 사용 분야
+ * - 결제 시스템
+ * - 알고리즘 교체
+ */
+public class StrategyCall {
+}
